f419b4c1966891f3467fa145b9bee1e03b0705c5
MadalinaX <127847333+MadalinaX@users.noreply.github.com>
2025-11-10 20:39:53 +0200

    Created the entities

---
+++ src/main/java/com/parking/parkinglot/entities/Car.java
@@ -0,0 +1,54 @@
+package com.parking.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "licensePLate", nullable = false)
+    private String licensePLate;
+
+    public String getLicensePLate() {
+        return licensePLate;
+    }
+
+    public void setLicensePLate(String licensePLate) {
+        this.licensePLate = licensePLate;
+    }
+
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(String parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    @Column(name = "parkingSpot", nullable = false)
+    private String parkingSpot;
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
